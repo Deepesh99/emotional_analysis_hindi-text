476a7459297f0c7c4ecfdee35f3dade7e8896f76
Nair Deepesh <33749311+Deepesh99@users.noreply.github.com>
2021-04-28 15:51:36 +0530

    lemma update 1.0

--- app.py
+++ app.py
@@ -25,35 +25,38 @@ def predict():
     # get data
     data = request.get_json(force=True)
     sent = data['comment']
-    print("sent",type(sent))
-    #print(sent)
+    ###print("sent",type(sent))
+    ###print(sent)
     res={}
-    print("res",type(res))
+    ###print("res",type(res))
     af_pre=preprocessing_hi(sent)
-    print("af_pre",type(af_pre))
-    print(af_pre)
-    print("hello")
+    ###print("af_pre",type(af_pre))
+    ###print(af_pre)
+    ###print("hello")
     res['after']=af_pre
     res['before']=sent
-    print("res",type(res))
+    ###print("res",type(res))
     return jsonify(res)
 
 
 def preprocessing_hi(text_hi):
     
     tweet_hi = []
-    print("tweet_hi",type(tweet_hi))
+    tweet_bi = []
+    ###print("tweet_hi",type(tweet_hi))
     tokenized_text = nlp_hi(text_hi)
-    print("tt",type(tokenized_text))
+    ###print("tt",type(tokenized_text))
     for token in tokenized_text:
         if(token.text!='\n\n' and not token.is_stop and not token.is_punct and not token.is_space and not token.like_email and not token.is_digit and not token.is_quote and not token.is_alpha and not token.like_url):
-              #tweet_hi.append(token.lemma_)
+              print(token)
+              print(token.lemma_)
+              tweet_bi.append(str(token.lemma_))
               tweet_hi.append(str(token))
-    print(tweet_hi)
-    print(type(tweet_hi[0]))
+    print(tweet_bi)
+    print(type(tweet_bi[0]))
     sep = ' '
     tweet = sep.join(tweet_hi)
-    print("tweet",type(tweet))
+    ###print("tweet",type(tweet))
     print(tweet)
     
     return tweet
