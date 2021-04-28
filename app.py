e55b1f18856b67d599a82ee92b5b9417c51be110
Nair Deepesh <33749311+Deepesh99@users.noreply.github.com>
2021-04-28 14:28:54 +0530

    added extra print comment for debugging

--- app.py
+++ app.py
@@ -25,34 +25,32 @@ def predict():
     # get data
     data = request.get_json(force=True)
     sent = data['comment']
+    print("sent",type(sent))
     res={}
+    print("res",type(res))
     af_pre=preprocessing_hi(sent)
+    print("af_pre",type(af_pre))
     print(af_pre)
     print("hello")
     res['after']=af_pre
     res['before']=sent
+    print("res",type(res))
     return jsonify(res)
 
 
 def preprocessing_hi(text_hi):
-  tweet_hi = []
-  tokenized_text = nlp_hi(text_hi)
-  for token in tokenized_text:
-    if(token.text!='\n\n' 
-        and not token.is_stop 
-        and not token.is_punct 
-        and not token.is_space 
-        and not token.like_email
-        and not token.is_digit
-        and not token.is_quote
-        and not token.is_alpha
-        and not token.like_url):
-        
-        tweet_hi.append(token.lemma_)  
-  tweet = ' '.join([token  for token in tweet_hi])
-
-  print(tweet)
-  print(type(tweet))
-  return tweet
+    
+    tweet_hi = []
+    print("tweet_hi",type(tweet_hi))
+    tokenized_text = nlp_hi(text_hi)
+    print("tt",type(tokenized_text))
+    for token in tokenized_text:
+        if(token.text!='\n\n' and not token.is_stop and not token.is_punct and not token.is_space and not token.like_email and not token.is_digit and not token.is_quote and not token.is_alpha and not token.like_url):
+              tweet_hi.append(token.lemma_)  
+    tweet = ' '.join([token  for token in tweet_hi])
+    print("tweet",type(tweet))
+    print(tweet)
+    
+    return tweet
 if __name__ == "__main__":
     app.run(port = 5000, debug=True)
